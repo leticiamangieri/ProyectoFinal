5c023c44450609c88239b7c4a94a29f178e5b368
facundopereirasantos <facundopereirasantos@gmail.com>
2025-08-21 18:59:14 -0300

    Cambio css
    Prueba

--- js/products.js
+++ js/products.js
@@ -1,21 +1,40 @@
-const div=document.getElementById("info");
+const div = document.getElementById("info");
 
-let catID=101;
-fetch (`https://japceibal.github.io/emercado-api/cats_products/${catID}.json`)
-    .then (res =>{
+let catID = 101;
+fetch(`https://japceibal.github.io/emercado-api/cats_products/${catID}.json`)
+    .then(res => {
         console.log(res);
         if (!res.ok) throw new Error("No encontrado");
         return res.json();
     })
-    .then (data =>{
+    .then(data => {
         console.log(data);
-        div.innerHTML+= `<h1>${data.catName}</h1>`
-        let i=0;
-        for (i=0;i<data.products.length;i++){
-            div.innerHTML+=`<p id="nombreProd">${data.products[i].name}</p>
-                            <p id="descProd">${data.products[i].description}</p>
-                            <p id="precioProd">${data.products[i].currency + ' '+data.products[i].cost}</p>
-                            <p id="cantDisponible">Cantidad de articulos disponibles: ${data.products[i].soldCount}</p>
-                          <img src="${data.products[i].image}" alt="Imagen del producto" class="imgProd">`
+        div.innerHTML = `<h1>${data.catName}</h1>`;
+        
+     
+        const productosContainer = document.createElement('div');
+        productosContainer.style.display = 'contents';
+        div.appendChild(productosContainer);
+        
+        let i = 0;
+        for (i = 0; i < data.products.length; i++) {
+        
+            const tarjeta = document.createElement('div');
+            tarjeta.className = 'producto-tarjeta';
+            
+        
+            tarjeta.innerHTML = `
+                <img src="${data.products[i].image}" alt="Imagen del producto" class="producto-imagen">
+                <p class="producto-nombre">${data.products[i].name}</p>
+                <p class="producto-descripcion">${data.products[i].description}</p>
+                <p class="producto-precio">${data.products[i].currency} ${data.products[i].cost}</p>
+                <p class="producto-cantidad">Cantidad de artículos disponibles: ${data.products[i].soldCount}</p>
+            `;
+            
+            productosContainer.appendChild(tarjeta);
         }
     })
+    .catch(error => {
+        console.error('Error:', error);
+        div.innerHTML += `<p>Error al cargar los productos: ${error.message}</p>`;
+    });
